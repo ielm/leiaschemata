d7aaac8fedf73abac156f96578bb77988097b01d
ielm <leoni@rpi.edu>
2020-02-17 14:03:17 -0500

    update formatting and add search

--- schema/api.py
+++ schema/api.py
@@ -6,14 +6,10 @@ class SchemaAPI:
     def __init__(self):
         self.collection = schema.management.handle()
 
-    def get_schema(self,
-                   sense: str,
-                   cat: Union[None, str] = None) -> dict:
-        schema_filter = {"$or": [{"SPEECH-ACT": sense}]}
+    def get_schema(self, tag: str, cat: Union[None, str] = None) -> dict:
+        schema_filter = {"$or": [{"TAG": tag}]}
 
-        filter = {
-            "$and": [schema_filter]
-        }
+        filter = {"$and": [schema_filter]}
 
         if cat is not None:
             filter = {"$and": [{"CAT": cat}]}
@@ -21,7 +17,7 @@ class SchemaAPI:
         results = self.collection.find(filter)
 
         if results.count() == 0:
-            raise Exception(f"Unknown schema {sense}.")
+            raise Exception(f"Unknown schema {tag}.")
 
         out = {}
         for r in results:
@@ -30,15 +26,57 @@ class SchemaAPI:
 
         return out
 
-    def list_senses(self, word: str) -> List[dict]:
-        pass  # TODO implement list senses for schemata
+    def list_senses(self, tag: str) -> List[dict]:
+        match = {"$match": {"TAG": tag}}
+
+        group = {"$group": {"_id": tag, "senses": {"$push": "$SENSE"}}}
+
+        aggregation = [match, group]
+
+        results = list(self.collection.aggregate(aggregation))
+
+        if len(results) == 0:
+            raise Exception(f"Unknown schema {tag}.")
+
+        return results[0]["senses"]
 
     def get_sense(self, sense: str) -> dict:
         entry = self.collection.find_one({"SENSE": sense})
 
         if entry is None:
-            raise Exception("Unknown sense " + sense + ".")
+            raise Exception(f"Unknown schema sense {sense}.")
 
         del entry["_id"]
 
         return entry
+
+    def search(self,
+               partial_name: str,
+               include_fields: Union[List[str], None] = None,
+               page_size=100,
+               page: Union[int, None] = None) -> List[dict]:
+
+        regex = f".*{partial_name}.*" if partial_name != "*" else ".*"
+        match = {"$match": {"SENSE": {"$regex": regex}}}
+        sort = {"$sort": {"SENSE": 1}}
+        aggregation = [match, sort]
+
+        if include_fields is not None:
+            project = {"$project": {"SENSE": 1}}
+
+            for field in include_fields:
+                project["$project"][field] = 1
+
+            aggregation.append(project)
+
+        if page is not None:
+            skip = {"$skip": (page * page_size)}
+            limit = {"$limit": page_size}
+            aggregation.append(skip)
+            aggregation.append(limit)
+
+        results = list(self.collection.aggregate(aggregation))
+        for r in results:
+            del r["_id"]
+
+        return results
