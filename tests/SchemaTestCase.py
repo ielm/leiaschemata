7c4fb6f6bd555a83af48142faa66052b4a25f827
ielm <leoni@rpi.edu>
2020-02-22 21:45:37 -0500

    formatting

--- tests/SchemaTestCase.py
+++ tests/SchemaTestCase.py
@@ -8,7 +8,7 @@ from pprint import pprint
 class SchemaTestCase(unittest.TestCase):
 
     def test_schema_build(self):
-        schema = Repo().get_cat("REQ_ACT")
+        schema = Repo().get_cat("REQUEST_ACTION")
         schema = Schema.build(schema)
         for elem in schema.elements():
             print(f"\n{elem.id}")
