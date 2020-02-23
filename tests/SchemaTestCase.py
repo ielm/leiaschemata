0b5d6930f9959e584512f599aea2a420c95cdc83
ielm <leoni@rpi.edu>
2020-02-22 20:08:41 -0500

    add tests for schema

---
+++ tests/SchemaTestCase.py
@@ -0,0 +1,15 @@
+from schema.api import SchemaAPI
+from schema.repository import Repo
+from schema.schema import Schema
+import unittest
+from pprint import pprint
+
+
+class SchemaTestCase(unittest.TestCase):
+
+    def test_schema_build(self):
+        schema = Repo().get_cat("REQ_ACT")
+        schema = Schema.build(schema)
+        for elem in schema.elements():
+            print(f"\n{elem.id}")
+            pprint(elem.debug())
