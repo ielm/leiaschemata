9baab33bc4a6a21eabb1e91a78632f1d6ee85af7
ielm <leoni@rpi.edu>
2020-02-22 21:44:27 -0500

    implement schema type

--- schema/schema.py
+++ schema/schema.py
@@ -1,19 +1,10 @@
 from ontograph.Frame import Frame
-from ontograph.Space import Space
 from ontoagent.engine.signal import Signal
-from typing import Dict, List, Union
+from typing import List
 from collections import OrderedDict
 
 
-class Element(Frame):
-
-    def null(self):
-        pass
-
-
 class Schema(Signal):
-    # Has an anchor and a root. The root is the speech-act of the schema, which has
-    # slots for all elements in the schema such as the SUBJ, HEAD, and DOBJ.
 
     @classmethod
     def build(cls, sdict: OrderedDict):
@@ -31,16 +22,22 @@ class Schema(Signal):
         return s
 
     def _build(self, content):
+
+        element_vars = {}
+
         def _construct_element(_id, _space, _content):
-            elem = Element(f"@{_space}.{_id}.?")
+            elem = Frame(f"@{_space}.{_id}.?")
 
             for item in list(_content["SYN-STRUC"].items()):
-                elem[item[0]] = item[1]
+                elem[item[0]] = item[1]  # add syntactic constraints
+                if item[0] == "ROOT":
+                    element_vars[item[1]] = elem
             for item in list(_content["SEM-STRUC"].items()):
-                elem["SEM"] = item[0]
-                if item[1]:
+                elem["SEM"] = item[0]  # add semantic constraint
+                if item[1]:  # if case-role constraints exist
                     for relation in item[1].items():
-                        elem["SEM"][relation[0]] = list(relation[1].items())[0][1]
+                        elem["SEM"][relation[0]] = list(
+                            relation[1].items())[0][1]
             return elem
 
         for e in content["SYN-STRUC"]:
@@ -49,17 +46,19 @@ class Schema(Signal):
                 "SEM-STRUC": content["SEM-STRUC"][e]
             }
             element = _construct_element(e, self.space().name, strucs)
+
             self.root()[e] = element
             self.add_element(element)
+
+        for e in self.elements():
+            for f in e.get_slot("SEM").facets():
+                if list(f.list())[0] in element_vars.keys():
+                    e.get_slot("SEM").get_facet(f.type).assign_filler(
+                        element_vars[list(f.list())[0]])
 
     def elements(self) -> List[Frame]:
         return list(self.anchor["HAS-ELEMENT"])
 
-    def add_element(self, element: Element):
+    def add_element(self, element: Frame):
         self.anchor["HAS-ELEMENT"] += element
-
-
-class Filter:
-
-    def __init__(self):
-        pass
+        self.add_constituent(element)
