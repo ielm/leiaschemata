4b1411dc78229ae0b93a645c76b2a6bab6bf1a4b
ielm <leoni@rpi.edu>
2020-07-13 17:05:40 -0400

    backup before switch

--- tests/SchemaTestCase.py
+++ tests/SchemaTestCase.py
@@ -1,30 +1,41 @@
-from ontoagent.engine.signal import TMR
+from ontoagent.engine.xmr import TMR
+from ontograph.Frame import Frame
+from ontograph_ontolang.OntoLang import OntoLang
+from ontogen.schema import link_tmr
 from schema.api import SchemaAPI
 from schema.repository import Repo
-from schema.schema import Schema
+from schema.newschema import Schema
 from lex.lexicon import Lexicon
 import unittest
-from ontoagent.utils.analysis import TextAnalyzer
+# from ontoagent.utils.analysis import TextAnalyzer
 from pprint import pprint
 
 
 class SchemaTestCase(unittest.TestCase):
 
     def test_schema_build(self):
-        analyzer = TextAnalyzer()
-        with open('resources/GetTheRedBlock.knowledge', 'r') as ontosem:
-            analyzer.cache("Get the red block.", str(ontosem.read()))
-        tmr = analyzer.to_signal("Get the red block.")
+        # analyzer = TextAnalyzer()
+        with open('tests/resources/GetTheRedBlock.knowledge', 'r') as ontosem:
+            # analyzer.cache("Get the red block.", str(ontosem.read()))
+            OntoLang().run(str(ontosem.read()))
+        # tmr = analyzer.to_signal("Get the red block.")
+        tmr = TMR.instance()
+        tmr.set_root(Frame("@TMR#1.REQUEST-ACTION.1"))
+        # print(tmr.root().debug())
         repo = Repo().get_cat(tmr.root().id.split(".")[1].replace("-", "_"))
         schemata = {}
         for key in repo.keys():
-            s = Schema.build(repo[key], tmr)
+            s = Schema.instance()
+            s.populate(repo[key])
+            s = link_tmr(s, tmr)
             schemata[key] = s
         # for i, s in zip(schemata.keys(), schemata.items()):
-        #     print(f"{i}: {s[1].debug()}")
+            # print(f"{i}: {s[1].debug()}")
         self.assertEqual(2, len(schemata))
         schema_keys = [str(key) for key in schemata.keys()]
         self.assertEqual(len(schema_keys), 2)
         self.assertEqual(schema_keys[0], "DITRANSITIVE-REQUEST_ACTION1")
         self.assertEqual(schema_keys[1], "MONOTRANSITIVE-REQUEST_ACTION1")
+
+        pprint(schemata)
 
